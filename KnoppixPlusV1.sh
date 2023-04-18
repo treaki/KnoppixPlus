edd59b665438d75955342534c7c13a279e37f098
treaki <treaki@gmail.com>
2023-04-18 07:17:55 +0200

    anydesk erster versuch...

--- KnoppixPlusV1.sh
+++ KnoppixPlusV1.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 echo adding KnoppixPlusV1 by treaki...
 echo adding KnoppixPlusV1 by treaki... >> /tmp/plusdlog
+date >> /tmp/plusdlog
+echo downloading and adding anydesk... >> /tmp/plusdlog
+
+cd /tmp
+wget https://freeshell.de/treaki/myKnoppix/anydesk.sh
+bash anydesk.sh
+cd -
+date >> /tmp/plusdlog
+echo eoanyscript >> /tmp/plusdlog
+
 
 #just for testing purposes
